592200150839be42712e7c4f7c5095da5d55f854
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-31 21:16:03 +0200

    Query Expression& deep understanding of IEnumberable

---
+++ LINQ ITI/Query Expression&IEnumerable&Extension/Linq pt2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_pt2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> lst = new List<int>() { 1 ,2,3,4,5};
+
+		  //1-	//static function member in enumerable class
+			var linq1= Enumerable.Where(lst, i => i % 2 == 0);
+
+		  //2-	//extension method
+		 var linq2=	lst.Where(i => i % 2==0);
+
+
+		  //3-	//query syntax , query expression
+			var linq3 = from i in lst
+						where i % 2 == 0
+						select i;
+
+		//1,2,3 does same code and have same execution , (translated to the same execution from the compiler)
+
+
+
+		}
+	}
+}
