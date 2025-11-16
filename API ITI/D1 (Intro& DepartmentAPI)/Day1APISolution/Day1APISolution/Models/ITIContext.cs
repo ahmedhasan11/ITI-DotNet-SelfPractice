67d000499332c0e6caaf37447e6156f63a587f28
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-11-16 15:37:21 +0200

    API Day 1

---
+++ API ITI/D1 (Intro& DepartmentAPI)/Day1APISolution/Day1APISolution/Models/ITIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day1APISolution.Models
+{
+	public class ITIContext:DbContext
+	{
+		public DbSet<Department> Departments { get; set; }
+
+		public ITIContext(DbContextOptions<ITIContext> options):base(options)
+		{
+
+		}
+	}
+}
