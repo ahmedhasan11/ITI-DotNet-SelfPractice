bb160078483d7cece952d0c19a5cd7bc4a45493f
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-11-18 11:35:34 +0200

    DTO& ModelBinding& Consume API using HTML& .net Console & Cors policy

---
+++ API ITI/D2 (DTO& Consume API)/Model Binding & DTO/Day1APISolution/Day1APISolution/Models/ITIContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day1APISolution.Models
+{
+	public class ITIContext:DbContext
+	{
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+
+		public ITIContext(DbContextOptions<ITIContext> options):base(options)
+		{
+
+		}
+	}
+}
---
+++ API ITI/D2 (DTO& Consume API)/Model Binding & DTO/Day1APISolution/Day1APISolution/Program.cs
@@ -0,0 +1,41 @@
+
+using Day1APISolution.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day1APISolution
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ITIContext>(options => 
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+                });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
