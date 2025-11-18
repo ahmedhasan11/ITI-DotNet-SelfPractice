c5fdcb92aa13dde8f49fa882cd10488ef868b52b
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-11-18 14:57:15 +0200

    Consuming api from a .net project

---
+++ API ITI/D2 (DTO& Consume API)/Consume API [.NET Project]/Day1APISolution/Day1APISolution/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day1APISolution.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string? Addresss { get; set; }
+		[ForeignKey("Department")]
+		public int DepartmentId { get; set; }
+
+		public Department? Department { get; set; }
+	}
+}
---
+++ API ITI/D2 (DTO& Consume API)/Consume API [.NET Project]/Day1APISolution/ServiceConsumer/Dept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceConsumer
+{
+	public class Dept
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string ManagerName { get; set; }
+	}
+}
